2dd457a7f551a2bdf6004572533b8647185bed91
Marc Torrent <marc.torrent@cea.fr>
2020-09-19 16:54:21 +0200

    Upgrade to Abinit 9.0.4

--- Formula/abinit.rb
+++ Formula/abinit.rb
@@ -1,127 +1,141 @@
 class Abinit < Formula
   desc "Atomic-scale first-principles simulation software"
   homepage "https://www.abinit.org/"
-  url "https://www.abinit.org/sites/default/files/packages/abinit-8.10.3.tar.gz"
-  sha256 "ed626424b4472b93256622fbb9c7645fa3ffb693d4b444b07d488771ea7eaa75"
-  revision 1
+  url "https://www.abinit.org/sites/default/files/packages/abinit-9.0.4.tar.gz"
+  sha256 "7aedf5ec2108acf36d097dfc3025e9ef5416623a0accc482495ec4776c707b29"
   # tag "chemistry"
-  # doi "10.1016/j.cpc.2016.04.003"
+  # doi "10.1016/j.cpc.2019.107042"
 
   bottle do
     root_url "http://forge.abinit.org/homebrew"
     cellar :any
-    sha256 "a8c47de834f004373686f444052464d6cfd857f73e2537fac9ce132f41cc26ac" => :catalina
-    sha256 "0fd0fb8fa3bfd3c430cc0d93f3d096ce4bf12aeb379d428cbb24b5bb5daca8a3" => :mojave
-    sha256 "fbfbcd2053d2f8632e19f48bb5f9093c3d058434e5f8d2a4057fa1435d02c33e" => :high_sierra
-end
+    sha256 "88015de13c7d4dea972af98027886b544a87e6dcca1a071e7a6b075fb74968fc" => :catalina
+    sha256 "d6f9aae039ac8c9c4ac1b9fa33b43643dad2db37a1b9913e58325daf070e401e" => :mojave
+    sha256 "17434778ab928ae89e139413b9f2cbd41f12892395c9026042e5943652786224" => :high_sierra
+  end
 
   option "without-openmp", "Disable OpenMP multithreading"
   option "without-test", "Skip build-time quick tests (not recommended)"
   option "with-testsuite", "Install script to run full test suite (see: brew test abinit)"
 
-  depends_on "gcc" if OS.mac? # for gfortran
-  depends_on "gcc" if build.with? "openmp"
+  depends_on "gcc"
+  depends_on "libxc"
+  depends_on "netcdf"
   depends_on "open-mpi"
-  depends_on "fftw" => :recommended
-  depends_on "libxc" => :recommended
-  depends_on "netcdf" => :recommended
   if OS.mac?
     depends_on "veclibfort"
-    depends_on "scalapack" => :recommended
+  else
+    depends_on "lapack"
   end
+  depends_on "fftw" => :recommended
+  depends_on "hdf5-parallel" => :recommended
+  depends_on "libxml2" => :recommended
+  depends_on "scalapack" => :recommended
+  depends_on "wannier90" => :recommended
 
-# From libxc3 to libXC4
-  patch do
-    url "https://github.com/abinit/homebrew-tap/raw/master/Formula/libxc_3to4_patch.diff"
-    sha256 "257a493f6ab3079886631a488f52f2d4d1e0559c6553456bd21c0c3070311b41"
-  end
-
-# Multibinit 8.10.3 needs to be patched with gcc9.3
-  patch :DATA
+  conflicts_with "abinit8", because: "abinit 9 and abinit 8 share the same executables"
 
   def install
-
     ENV.delete "CC"
     ENV.delete "CXX"
     ENV.delete "F77"
     ENV.delete "FC"
 
-    args = %W[
+    compilers = %w[
       CC=mpicc
       CXX=mpicxx
       FC=mpif90
-      --prefix=#{prefix}
-      --enable-mpi=yes
-      --with-mpi-prefix=#{HOMEBREW_PREFIX}
-      --enable-optim=standard
-      --enable-gw-dpc
     ]
+    # Workaround to compile Abinit 9.0.4 with gcc10+
+    compilers << "FCFLAGS_EXTRA=-fallow-argument-mismatch"
+
+    args = %W[
+      --prefix=#{prefix}
+      --with-mpi=yes
+      --enable-mpi-inplace=yes
+      --with-optim-flavor=standard
+      --with-netcdf=#{Formula["netcdf"].opt_prefix}
+      --with-netcdf-fortran=#{Formula["netcdf"].opt_prefix}
+      --with-libxc=#{Formula["libxc"].opt_prefix}
+      --enable-zdot-bugfix=yes
+    ]
+
+    libs = %w[]
+
     args << ("--enable-openmp=" + (build.with?("openmp") ? "yes" : "no"))
 
-    trio_flavor = "none"
-    dft_flavor = "none"
-    linalg_flavor = "none"
-    fft_flavor = "none"
-
-    if OS.mac?
-      if build.with? "scalapack"
-        linalg_flavor = "custom+scalapack"
-        args << "--with-linalg-libs=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort -L#{Formula["scalapack"].opt_lib} -lscalapack"
+    if build.with? "scalapack"
+      args << "--with-linalg-flavor=netlib"
+      libs << if OS.mac?
+        "LINALG_LIBS=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort " \
+        "-L#{Formula["scalapack"].opt_lib} -lscalapack"
       else
-        linalg_flavor = "custom"
-        args << "--with-linalg-libs=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort"
+        "LINALG_LIBS=-L#{Formula["lapack"].opt_lib} -lblas -llapack " \
+        "-L#{Formula["scalapack"].opt_lib} -lscalapack"
+      end
+    else
+      args << "--with-linalg-flavor=none"
+      libs << if OS.mac?
+        "LINALG_LIBS=-L#{Formula["veclibfort"].opt_lib} -lvecLibFort"
+      else
+        "LINALG_LIBS=-L#{Formula["lapack"].opt_lib} -lblas -llapack"
       end
     end
 
-    if build.with? "netcdf"
-      trio_flavor = "netcdf"
-      args << "--with-netcdf-incs=-I#{Formula["netcdf"].opt_include}"
-      args << "--with-netcdf-libs=-L#{Formula["netcdf"].opt_lib} -lnetcdff -lnetcdf"
-    end
-
-    # Need to link against single precision as well,
-    #  see https://trac.macports.org/ticket/45617
-    #  and http://forum.abinit.org/viewtopic.php?f=3&t=2631
     if build.with? "fftw"
-      fft_flavor = "fftw3"
-      args << "--with-fft-incs=-I#{Formula["fftw"].opt_include}"
-      args << "--with-fft-libs=-L#{Formula["fftw"].opt_lib} -lfftw3 -lfftw3f -lfftw3_mpi -lfftw3f_mpi"
+      args << "--with-fft-flavor=fftw3"
+      libs << "FFTW3_FCFLAGS=-I#{Formula["fftw"].opt_include}"
+      libs << "FFTW3_LIBS=-L#{Formula["fftw"].opt_lib} " \
+              "-lfftw3_threads -lfftw3 -lfftw3f -lfftw3_mpi -lfftw3f_mpi"
+    else
+      args << "--with-fft-flavor=goedecker"
     end
 
-    if build.with? "libxc"
-      dft_flavor="libxc"
-      args << "--with-libxc-incs=-I#{Formula["libxc"].opt_include}"
-      args << "--with-libxc-libs=-L#{Formula["libxc"].opt_lib} -lxcf90 -lxc"
-    end
+    args << "--with-libxml2=#{Formula["libxml2"].opt_prefix}" if build.with? "libxml2"
+    args << "--with-hdf5=#{Formula["hdf5-parallel"].opt_prefix}" if build.with? "hdf5-parallel"
+    args << "--with-wannier90=#{Formula["wannier90"].opt_prefix}" if build.with? "wannier90"
 
-    args << "--with-linalg-flavor=#{linalg_flavor}"
-    args << "--with-fft-flavor=#{fft_flavor}"
-    args << "--with-trio-flavor=#{trio_flavor}"
-    args << "--with-dft-flavor=#{dft_flavor}"
-
-    system "./configure", *args
+    system "./configure", *args, *libs, *compilers
     system "make"
 
     if build.with? "test"
-      #Execute quick tests
-      system "./tests/runtests.py built-in fast 2>&1 | tee make-check.log"
+      # Execute quick tests
+      system "./tests/runtests.py built-in fast &> make-check.log"
+      system "grep -A1 Suite make-check.log"
       ohai `grep ", succeeded:" "make-check.log"`.chomp
       prefix.install "make-check.log"
     elsif build.with? "testsuite"
-      #Generate test database only
-      system "./tests/runtests.py 2>&1 fast[00] >/dev/null"
+      # Generate test database only
+      system "./tests/runtests.py fast[00] &> /dev/null"
     end
 
     system "make", "install"
 
     if build.with? "testsuite"
-      #Create a wrapper to runtests.py script
-      test_path = share/"abinit-test"
+      test_path = share/"tests"
+      test_dir = Pathname.new(test_path.to_s)
+      # Copy tests directory
+      share.install "#{buildpath}/tests"
+      # Delete some files
+      rm_rf "#{test_path}/config"
+      rm_rf "#{test_path}/config.*"
+      rm_rf "#{test_path}/configure.*"
+      rm_rf "#{test_path}/Makefile.*"
+      rm_rf "#{test_path}/autogen.sh"
+      rm_rf "#{test_path}/wipeout.sh"
+      # Copy some needed files
+      test_dir.install "config.h"
+      Pathname.new("#{test_path}/runtests.py").chmod 0555
+      # Create symlinks to have a fake test environment
+      mkdir_p "#{test_path}/src"
+      cd "#{test_path}/src"
+      ln_s bin.relative_path_from("#{test_path}/src"), "98_main", force: true
+      # Create a wrapper to runtests.py script
       test_file = File.new("abinit-runtests", "w")
       test_file.puts "#!/bin/sh"
-      revision == 0 ? ver = "#{version}" : ver = "#{version}_#{revision}"
+      ver = revision.zero? ? version.to_s : "#{version}_#{revision}"
       test_file.puts "SHAREDIR=\`brew --cellar\`\"/#{name}/#{ver}/share\""
-      test_file.puts "TESTDIR=${SHAREDIR}\"/abinit-test\""
+      test_file.puts "TESTDIR=${SHAREDIR}\"/tests\""
       test_file.puts "if [ -w \"${TESTDIR}/test_suite.cpkl\" ];then"
       test_file.puts " PYTHONPATH=${SHAREDIR}\":\"${PYTHONPATH} ${TESTDIR}\"/runtests.py\" -b\"${TESTDIR}\" $@"
       test_file.puts "else"
@@ -129,20 +143,9 @@ end
       test_file.puts "fi"
       test_file.close
       bin.install "abinit-runtests"
-      #Copy some needed files
-      test_dir = Pathname.new("#{test_path}")
-      test_dir.install "config.h"
-      test_dir.install "tests/test_suite.cpkl"
-      Pathname.new("#{test_path}/runtests.py").chmod 0555
-      #Create symlinks to have a fake test environment
-      cd share
-      ln_s "abinit-test", "tests", force: true
-      mkdir_p "#{test_path}/src"
-      cd "#{test_path}/src"
-      ln_s bin.relative_path_from("#{test_path}/src"), "98_main", force: true
+      # Go back into buildpath
       cd buildpath
     end
-
   end
 
   test do
@@ -150,11 +153,13 @@ end
     if build.with? "testsuite"
       system "#{bin}/abinit-runtests", "--help"
       puts
-      puts "The entire ABINIT test suite is available"
+      puts "The entire ABINIT test suite has been copied into:"
+      puts "#{share}/tests"
+      puts
+      puts "This ABINIT test suite is available"
       puts "thanks to the 'abinit-runtests' script."
       puts "Type 'abinit-runtests --help' to learn more."
-      puts "Note that you need write access to #{share}/abinit-test"
-      puts "and that some libXC tests (based on libXC v3) will failed." 
+      puts "Note that you need write access to #{share}/tests."
     else
       puts
       puts "ABINIT test suite is not available because it"
@@ -162,66 +167,4 @@ end
       puts "Action: install with 'brew install abinit --with-testsuite'."
     end
   end
-
 end
-
-__END__
-diff --git a/src/78_effpot/m_polynomial_coeff.F90 b/src/78_effpot/m_polynomial_coeff.F90
-index 2eb330b..f5aade1 100644
---- a/src/78_effpot/m_polynomial_coeff.F90
-+++ b/src/78_effpot/m_polynomial_coeff.F90
-@@ -2502,8 +2502,8 @@ recursive subroutine computeNorder(cell,coeffs_out,compatibleCoeffs,list_coeff,l
- 
- !Arguments ---------------------------------------------
- !scalar
-- integer,intent(in) :: natom,ncoeff,power_disp,power_disp_min,power_disp_max,ncoeff_out,nsym,nrpt,nstr
-- integer,intent(inout) :: icoeff,icoeff_tot
-+ integer,intent(in) :: icoeff,natom,ncoeff,power_disp,power_disp_min,power_disp_max,ncoeff_out,nsym,nrpt,nstr
-+ integer,intent(inout) :: icoeff_tot
-  logical,optional,intent(in) :: compute,anharmstr,spcoupling,distributed
-  integer,optional,intent(in) :: nbody
- !arrays
-@@ -2554,7 +2554,9 @@ recursive subroutine computeNorder(cell,coeffs_out,compatibleCoeffs,list_coeff,l
- !    If the distance between the 2 coefficients is superior than the cut-off,
- !    we cycle
- !    If the power_disp is one, we need to set icoeff to icoeff1
--     if(power_disp==1) icoeff = icoeff1
-+     if(power_disp==1) then
-+       if(icoeff1 <= ncoeff .and. compatibleCoeffs(icoeff1,icoeff1)==0) cycle
-+     end if
- 
-      if(compatibleCoeffs(icoeff,icoeff1)==0) cycle
-
-@@ -2757,9 +2757,9 @@ recursive subroutine computeCombinationFromList(cell,compatibleCoeffs,list_coeff
- 
- !Arguments ---------------------------------------------
- !scalar
-- integer,intent(in) :: natom,ncoeff,power_disp,power_disp_min,power_disp_max
-+ integer,intent(in) :: icoeff,natom,ncoeff,power_disp,power_disp_min,power_disp_max
-  integer,intent(in) :: max_power_strain,nmodel,nsym,nrpt,nstr
-- integer,intent(inout) :: icoeff,nmodel_tot
-+ integer,intent(inout) :: nmodel_tot
-  logical,optional,intent(in) :: compute,anharmstr,spcoupling
-  integer,optional,intent(in) :: nbody
-  logical,optional,intent(in) :: only_odd_power,only_even_power
-@@ -2807,16 +2807,15 @@ recursive subroutine computeCombinationFromList(cell,compatibleCoeffs,list_coeff
- 
- !    If the power_disp is one, we need to set icoeff to icoeff1
-      if(power_disp==1) then
--       icoeff = icoeff1
--       if(compatibleCoeffs(icoeff,icoeff1)==0)then
-+       if(icoeff1<=ncoeff .and. compatibleCoeffs(icoeff,icoeff1)==0)then
-          compatible = .FALSE.
-        end if
-      end if
- !    If the distance between the 2 coefficients is superior than the cut-off, we cycle.
-     do icoeff2=1,power_disp-1
--      if(compatibleCoeffs(index_coeff(icoeff2),icoeff1)==0)then
--        compatible = .FALSE.
--      end if
-+      if(icoeff1 <= ncoeff .and. index_coeff(icoeff2) <=ncoeff)then
-+        if(compatibleCoeffs(index_coeff(icoeff2),icoeff1)==0) compatible=.FALSE.
-+      endif
-     end do
- 
-      if (.not.compatible) cycle !The distance is not compatible
