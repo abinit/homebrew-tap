93b0036d556d3478929091535c1d5017ffbd9c15
Jordan Bieder <jordan.bieder@uliege.be>
2021-01-08 20:52:37 +0100

    Update formula for agate 1.2.0 with readline

--- Formula/agate.rb
+++ Formula/agate.rb
@@ -1,8 +1,8 @@
 class Agate < Formula
   desc "Agate is a Graphical Analysis Tool Engine for DFT calculations"
   homepage "https://github.com/piti-diablotin/agate"
-  url "https://github.com/piti-diablotin/agate/releases/download/v1.1.2/agate-1.1.2.tar.gz"
-  sha256 "87ef3d24c862648989e44c1564a9aed8e2e8a2cb81cc69d3c38c1752d0391277"
+  url "https://github.com/piti-diablotin/agate/releases/download/v1.2.0/agate-1.2.0.tar.gz"
+  sha256 "867db9cbad666d479a999aa5b31dba25e40de3de628e08c455448bde9d2ca4d8"
   license "GPL-3.0"
 
   depends_on "automake" => :build
@@ -21,6 +21,8 @@ class Agate < Formula
   depends_on "libxml2" => :build
   depends_on "netcdf" => :build
   depends_on "yaml-cpp" => :build
+  depends_on "readline" => :build
+  depends_on "with-readline" => :build
 
   def install
     # Remove unrecognized options if warned by configure
