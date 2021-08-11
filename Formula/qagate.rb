f57a028319cdf175b6a16f6ebb77b6463e4c64d1
Jordan Bieder <jordan.bieder@uliege.be>
2021-08-11 08:15:18 +0200

    Update qagate to version 1.2.3

--- Formula/qagate.rb
+++ Formula/qagate.rb
@@ -1,8 +1,8 @@
 class Qagate < Formula
   desc "Qt interface for agate"
   homepage "https://github.com/piti-diablotin/qAgate"
-  url "https://github.com/piti-diablotin/qAgate/releases/download/v1.2.2/qAgate1.2.2.tar.gz"
-  sha256 "b1b70918c38288c0f9fea10e7b2762ec5638443eefc5992f084b7fe42365adf7"
+  url "https://github.com/piti-diablotin/qAgate/releases/download/v1.2.3/qAgate1.2.3.tar.gz"
+  sha256 "104a21533d20dd5ee61bb08278ea946aec2f7a1742a00490540bda10df26d02f"
   license "GPL-3.0"
 
   depends_on "agate" => [:build, 'without-gnuplot']
