befaae1a5e41027c2f536500b9ec72c51060c347
Jordan Bieder <jordan.bieder@uliege.be>
2021-08-11 08:22:51 +0200

    Update Cask to 1.2.3

--- Casks/qagate.rb
+++ Casks/qagate.rb
@@ -1,8 +1,8 @@
 cask "qagate" do
-  version "1.2.2"
-  sha256 "cffc2693f711558bd21aa8b65d89811511ffde0c246ead7c5c7fc39a500ad74e"
+  version "1.2.3"
+  sha256 "f4b90b19cb237d22758d643a66d0005d6b74506cb7061c97e6911747df0251fe"
 
-  url "https://github.com/piti-diablotin/qAgate/releases/download/v1.2.2/qAgate-1.2.2.dmg"
+  url "https://github.com/piti-diablotin/qAgate/releases/download/v1.2.3/qAgate-1.2.3.dmg"
   name "qAgate"
   desc "Qt interface for agate"
   homepage "https://github.com/piti-diablotin/qAgate"
