641233cc6b43a0f22dbd6f4952c0c1be03f74d01
Marc Torrent <marc.torrent.prive@club-internet.Fr>
2023-07-04 19:43:56 +0200

    Upgrade Abinit to v9.10.1

--- Formula/abinit.rb
+++ Formula/abinit.rb
@@ -1,20 +1,19 @@
 class Abinit < Formula
   desc "Atomic-scale first-principles simulation software"
   homepage "https://www.abinit.org/"
-  url "https://www.abinit.org/sites/default/files/packages/abinit-9.8.4.tar.gz"
-  sha256 "a086d5045f0093b432e6a044d5f71f7edf5a41a62d67b3677cb0751d330c564a"
+  url "https://www.abinit.org/sites/default/files/packages/abinit-9.10.1.tar.gz"
+  sha256 "136f0ffcc10b36b6e577132092d31e42b3c00f57098854fc15e5cf988b687997"
   license "GPL-3.0-only"
 
   bottle do
     root_url "http://forge.abinit.org/homebrew"
-    sha256 cellar: :any, arm64_ventura: "bea062467cb604bf111fc093bd11032c473c0bfdb7d40cf54b9a99bc243505b2"
-    sha256 cellar: :any, arm64_monterey: "383dc528cae73cf4d142c7837de63c641fc00f5a25689cda366aeed50db8832c"
-    sha256 cellar: :any, ventura: "4664de0c0776e52a686f2779fe894bead3990b3b83142ad3856c8e4803568eed"
-    sha256 cellar: :any, monterey: "027dc1774f6e74ee16750f9b1a5631a9db3c2c399c4e8f500c069b8c825b59a8"
-    sha256 cellar: :any, big_sur: "e04cbb8594c95d8ccf938d9f4f17c373741371ea58563e9da5e252182f4c98ab"
-    sha256 cellar: :any, catalina: "886ff3441e7b1e3298fcde075970c48f3e361d3e6ab089ffdac1b41d73544a82"
-    sha256 cellar: :any, mojave: "c770c6a316242ee5a7570dba8585fce39d862c043645b199adea642ec0860c8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ac2a0a6f2d8f14a2fab1b0fe882c74556f8c54e2268b8a1ca9c7ce194f9e448d"
+    sha256 cellar: :any, arm64_ventura: "480542841f6f5df4f672bf5d58731799388e873991d485d719b806fa1e63989c"
+    sha256 cellar: :any, arm64_monterey: "3bffdbbc33f4f47792a75b8d3dcd88d80694f7ad3d869c26b1754b0066512205"
+    sha256 cellar: :any, ventura: "4aac750d2d5b84f25fedb5e39e93ff250bcc266cb0934fd15cae6fa061e2223c"
+    sha256 cellar: :any, monterey: "ddb60d57c78b110808c45984b44e652376e26da8d5998ee3059a6cecaac032fe"
+    sha256 cellar: :any, big_sur: "828bc2e138a4a2e3c5e54586ede8390f856be6721a786e24d6d9731b65dfccd3"
+    sha256 cellar: :any, catalina: "f9ecfcedad1addc87d31918b54f8219b6ad819feafcce508ac6ccc96819379e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8faed673518b9b833b893a366ee5c770ce343818b2e0d9fdd2706471efb29a7e"
   end
 
   option "without-openmp", "Disable OpenMP multithreading"
@@ -33,9 +32,6 @@ class Abinit < Formula
   depends_on "netcdf-fortran-parallel" => :optional
 
   conflicts_with "abinit8", because: "abinit 9 and abinit 8 share the same executables"
-
-  # Abinit 9.8 needs to be patched with gcc13 (in m_gwls_GenerateEpsilon.F90)
-  patch :DATA
 
   def install
     ENV.delete "CC"
@@ -186,45 +182,3 @@ class Abinit < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/70_gw/m_gwls_GenerateEpsilon.F90 b/src/70_gw/m_gwls_GenerateEpsilon.F90
-index 2ea0f6c..70772ed 100644
---- a/src/70_gw/m_gwls_GenerateEpsilon.F90
-+++ b/src/70_gw/m_gwls_GenerateEpsilon.F90
-@@ -156,7 +156,7 @@ end subroutine driver_generate_dielectric_matrix
- !!
- !! SOURCE
- 
--subroutine GeneratePrintDielectricEigenvalues(epsilon_matrix_function,kmax,output_filename,Lbasis,alpha,beta)
-+subroutine GeneratePrintDielectricEigenvalues(epsilon_matrix_function,nseeds,kmax,output_filename,Lbasis,alpha,beta)
- !----------------------------------------------------------------------
- ! This routine computes the Lanczos approximate representation of the
- ! implicit dielectic operator and then diagonalizes the banded
-@@ -173,7 +173,7 @@ interface
-   end subroutine epsilon_matrix_function
- end interface
- 
--integer,       intent(in) :: kmax
-+integer,       intent(in) :: nseeds,kmax
- 
- character(*),  intent(in) :: output_filename
- 
-@@ -456,7 +456,7 @@ call set_dielectric_function_frequency([zero,zero])
- 
- call cpu_time(time1)
- output_filename = 'EIGENVALUES_EXACT.dat'
--call GeneratePrintDielectricEigenvalues(matrix_function_epsilon_k, kmax_exact, &
-+call GeneratePrintDielectricEigenvalues(matrix_function_epsilon_k, nseeds, kmax_exact, &
- output_filename, Lbasis_exact, alpha_exact, beta_exact)
- 
- 
-@@ -471,7 +471,7 @@ call write_timing_log(timing_string,time)
- call cpu_time(time1)
- call setup_Pk_model(zero,second_model_parameter)
- output_filename = 'EIGENVALUES_MODEL.dat'
--call GeneratePrintDielectricEigenvalues(matrix_function_epsilon_model_operator, kmax_model, output_filename,&
-+call GeneratePrintDielectricEigenvalues(matrix_function_epsilon_model_operator, nseeds, kmax_model, output_filename,&
- Lbasis_model, alpha_model, beta_model)
- call cpu_time(time2)
- time = time2-time1
